4fac7590853c5ba89831acef593c1b5914144a98
josAlba <soydesilla@hotmail.es>
2021-10-18 18:32:50 +0200

    [UPD] php 8.0

--- src/mysql.php
+++ src/mysql.php
@@ -2,68 +2,66 @@
 
 namespace mysqlconnect;
 
-class mysql{
+use mysqli;
+use mysqli_result;
+use stdClass;
 
-    private $link;
-	
-	public function _limpiar($texto)
-	{
-		return $this->link->real_escape_string($texto);
-	}
-	
-	public function __construct($host,$user,$passwd,$port,$db)
-	{
-		$V_principal['_BD'] = array();
-		//HOSTING BASE DE DATOS.
-		$V_principal['_BD']['BD_HOST']      = $host;
-		//USUARIO BASE DE DATOS.
-		$V_principal['_BD']['BD_USUARIO']   = $user;
-		//CONTRASENA BASE DE DATOS.
-		$V_principal['_BD']['BD_PASSWORD']  = $passwd;
-		//PUERTO BASE DE DATOS.
-		$V_principal['_BD']['BD_PUERTO']    = $port;
-		//BASE DE DATOS.
-        $V_principal['_BD']['BD_BD']        = $db;
-        
+class mysql
+{
+    private $db;
 
-		$this->link = new \mysqli($V_principal['_BD']['BD_HOST'], $V_principal['_BD']['BD_USUARIO'], $V_principal['_BD']['BD_PASSWORD'], $V_principal['_BD']['BD_BD'], $V_principal['_BD']['BD_PUERTO']);
-		$this->link->set_charset("utf8");
-	}
-	
-	public function __destruct() 
-	{
-		$this->link->close();
-	}
-	
-	public function _ultimo_id(){
-		return $this->link->insert_id;
-	}
-	
-	public function _db_consulta($sql)
-	{
-		
-		
-		$result = $this->link->query($sql);
-		if($result instanceof \mysqli_result)
-		{
-				$data = array();
+    /**
+     * @param string $text
+     * @return string
+     */
+    public function _limpiar(string $text): string
+    {
+        return $this->db->real_escape_string($text);
+    }
 
-				while ($row = $result->fetch_assoc()) {
-					$data[] = $row;
-				}
-				
-				$result1 = new \stdClass();
-				$result1->num_rows = $result->num_rows;
-				$result1->row = isset($data[0]) ? $data[0] : array();
-				$result1->rows = $data;
-				
-		}
-		else
-		{  	
-			return false;
-		}
-		//$this->link->close();
-		return $result1;
-	}
+    public function __construct($host, $user, $passwd, $port, $db)
+    {
+        $this->db = new mysqli(
+            $host,
+            $user,
+            $passwd,
+            $port,
+            $db);
+        $this->db->set_charset("utf8");
+    }
+
+    public function __destruct()
+    {
+        $this->db->close();
+    }
+
+    public function _ultimo_id()
+    {
+        return $this->db->insert_id;
+    }
+
+    /**
+     * @param string $sql
+     * @return stdClass|null
+     */
+    public function _db_consulta(string $sql): ?stdClass
+    {
+        $result = $this->db->query($sql);
+        if ($result instanceof mysqli_result) {
+            $data = array();
+
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+
+            $resultObject = new stdClass();
+            $resultObject->num_rows = $result->num_rows;
+            $resultObject->row = $data[0] ?? array();
+            $resultObject->rows = $data;
+        } else {
+            return null;
+        }
+        return $resultObject;
+    }
 
 }
